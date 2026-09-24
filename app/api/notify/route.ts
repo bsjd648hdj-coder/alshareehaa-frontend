94db481dc2f87df19d60aab922b166a16f8d31bd
Mohamed Samir Mouawad <114841411+mohamed-samir-dev@users.noreply.github.com>
2026-09-24 04:39:47 +0300

    FF

--- app/api/notify/route.ts
+++ app/api/notify/route.ts
@@ -49,7 +49,7 @@ export async function POST(req: NextRequest) {
   const formattedPhone = (whatsapp ?? "-").replace(/^(\+966|966)/, "0");
 
   const text = [
-    `🛒 متجر مؤسسة لمسه لبيع الشرائح`,
+    `🛒 متجر الشريحه الموثوقه لبيع الشرائح`,
     `🔖 Order ID: ${ltr}#${orderId}`,
     ``,
     `💲 Total Amount: ${ltr}${total} SAR`,
